1c66c96ce07006feddf1a84eb364449f967baa23
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-11-03 14:11:50 -0400

    adjusted countries controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,32 +1,25 @@
 class CountriesController < ProtectedController
   before_action :set_country, only: [:update, :destroy]
-
   # GET /countries
   def index
     @countries = current_user.countries
-
     render json: @countries
   end
-
   # GET /countries/1
   def show
-    render json: @country
+    @countries = Country.find(params[:id])
+        render json: @country
   end
-
   # POST /countries
   def create
 
-    #@country = Country.new(country_params)
-     @country = current_user.countries.build(country_params)
-    @country.user = current_user
-
+    @country = current_user.countries.build(country_params)
     if @country.save
-      render json: @country, status: :created
+      render json: @country, status: :created, location: @country
     else
       render json: @country.errors, status: :unprocessable_entity
     end
   end
-
   # PATCH/PUT /countries/1
   def update
     if @country.update(country_params)
@@ -36,20 +29,14 @@ class CountriesController < ProtectedController
     end
   end
 
-  # DELETE /countries/1
   def destroy
     @country.destroy
   end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_country
-    @country = current_user.countries.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def country_params
-    params.require(:country).permit(:name, :continent, :capital_city)
-  end
+    def set_country
+       @country = current_user.countries.find(params[:id])
+    end
+    def country_params
+      params.require(:country).permit(:name, :user_id)
+    end
 end
